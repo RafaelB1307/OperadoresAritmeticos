59015583768627a6dde10bb426c8909af60eb571
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-20 23:41:35 -0700

    Agregar archivos de proyecto.

---
+++ Suma/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+    internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int num1 = 8;
+                double num2 = 4.5;
+                double resultado;
+
+                //Variable string
+                string saludo = "Hola";
+                string nombre = " Rafael";
+
+                //Sumamos las dos variables
+                //resultado = num1 + num2;
+
+                //Sumando una variable y un dato puesto esplicitamente
+                resultado = num1 + 22;
+                //Console.WriteLine(resultado);
+
+                //Sumando o cocatenando dos cadenas almacenadas en variables
+                Console.WriteLine(saludo + nombre + " ¿Como estas? ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
+    }
+}
